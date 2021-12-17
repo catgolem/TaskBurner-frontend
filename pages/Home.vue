3506d56ca1f92e2f1f3b93f229a6cb298d3752d9
catgolem <kugi1255@icloud.com>
2021-12-18 00:40:04 +0900

    [modify] Home.vue のデザイン終了

--- pages/Home.vue
+++ pages/Home.vue
@@ -21,7 +21,7 @@
     <!-- メインのブロック -->
     <div style="height: 90vh;" class="grid grid-rows-3 grid-cols-2 gap-7 py-8 mx-10 opacity-80">
       <!-- 左のブロック（タスク一覧） -->
-      <div class="border-4 border-black row-span-3 col-span-1 shadow-lg rounded-lg overflow-hidden">
+      <div class="border-4 border-black row-span-3 col-span-1 shadow-lg rounded-lg overflow-hidden bg-gray-100">
         <!-- タイトル -->
         <div class="border-b-4 border-black flex justify-start text-shadow-sm align-middle ml-3 text-2xl font-black py-2">
           <img class="mr-2" src="../Assets/TaskIcon2.png" alt="リストアイコン">
@@ -157,7 +157,7 @@
             </div>
           </div>
           <!-- Task -->
-          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-200 border-b-4 border-dotted">
             <!-- 左 -->
             <div class="inline-block border-r-4 border-gray-600 pr-2">
               <div>
@@ -261,7 +261,7 @@
 
 
       <!-- 右側のブロック（Doneの部分） -->
-      <div class="row-span-1 col-span-1 border-4 border-black shadow-lg rounded-lg overflow-hidden">
+      <div class="row-span-1 col-span-1 border-4 border-black shadow-lg rounded-lg overflow-hidden bg-gray-100">
         <!-- タイトル -->
         <div class="border-b-4 border-black flex justify-start text-shadow-sm align-middle ml-3 text-2xl font-black py-2">
           <img class="mr-2" src="../Assets/TaskIcon2.png" alt="リストアイコン">
@@ -348,16 +348,117 @@
         </div>
       </div>
 
+
       <!-- 右側のブロック（ランキングのところ） -->
-      <div class="row-span-2 col-span-1 border-4 border-black shadow-lg rounded-lg">
+      <div class="row-span-2 col-span-1 border-4 border-black shadow-lg rounded-lg bg-red-300">
         <!-- タイトル -->
         <div class="border-b-4 border-black flex justify-start text-shadow-sm align-middle ml-3 text-2xl font-black py-2">
           <img class="mr-2" src="../Assets/TaskIcon2.png" alt="リストアイコン">
           Crazy Burners !!
         </div>
-      </div>
-      <!-- リスト -->
-      <div>
+        <!-- リスト -->
+        <div class="align-middle text-2xl font-black overscroll-auto overflow-auto pl-4 text-2xl font-black opacity-80 section">
+          <!-- Rank -->
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+            <!-- 左 -->
+            <div class="flex justify-start border-r-4 border-gray-600 pr-2">
+              <div class="pt-4">
+                <img class="inline-block" src="../Assets/champ.png">
+                <h1 class="inline-block">
+                  No.1
+                </h1>
+              </div>
+            </div> 
+            <!-- 右 -->
+            <div class="inline-brock">
+              <div class="ml-3">
+                You are the hottest man in the world!
+              </div>
+              <img class="inline-block ml-1 pl-3" src="../Assets/per.png" alt="完了">
+              <div class="ml-3 inline-block rounded-full align-middle">Cat</div>              
+            </div>
+          </div>
+          <!-- Rank -->
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+            <!-- 左 -->
+            <div class="flex justify-start border-r-4 border-gray-600 pr-2">
+              <div class="pt-4">
+                <img class="inline-block" src="../Assets/rank.png">
+                <h1 class="inline-block">
+                  No.2
+                </h1>
+              </div>
+            </div> 
+            <!-- 右 -->
+            <div class="inline-brock">
+              <div class="ml-3">
+                The Antarctic ice is melting because of your heat!
+              </div>
+              <img class="inline-block ml-1 pl-3" src="../Assets/per.png" alt="完了">
+              <div class="ml-3 inline-block rounded-full align-middle">Cat</div>              
+            </div>
+          </div>
+          <!-- Rank -->
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+            <!-- 左 -->
+            <div class="flex justify-start border-r-4 border-gray-600 pr-2">
+              <div class="pt-4">
+                <img class="inline-block" src="../Assets/rank.png">
+                <h1 class="inline-block">
+                  No.3
+                </h1>
+              </div>
+            </div> 
+            <!-- 右 -->
+            <div class="inline-brock">
+              <div class="ml-3">
+                The Arctic ice is melting because of your heat!
+              </div>
+              <img class="inline-block ml-1 pl-3" src="../Assets/per.png" alt="完了">
+              <div class="ml-3 inline-block rounded-full align-middle">Cat</div>              
+            </div>
+          </div>
+          <!-- Rank -->
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+            <!-- 左 -->
+            <div class="flex justify-start border-r-4 border-gray-600 pr-2">
+              <div class="pt-4">
+                <img class="inline-block" src="../Assets/Fire2.png">
+                <h1 class="inline-block">
+                  No.4
+                </h1>
+              </div>
+            </div> 
+            <!-- 右 -->
+            <div class="inline-brock">
+              <div class="ml-3">
+                Your heat is cheap!
+              </div>
+              <img class="inline-block ml-1 pl-3" src="../Assets/per.png" alt="完了">
+              <div class="ml-3 inline-block rounded-full align-middle">Cat</div>              
+            </div>
+          </div>
+          <!-- Rank -->
+          <div class="pb-2 pt-2 my-1 inline-block flex justify-start border-gray-00 border-b-4 border-dotted">
+            <!-- 左 -->
+            <div class="flex justify-start border-r-4 border-gray-600 pr-2">
+              <div class="pt-4">
+                <img class="inline-block" src="../Assets/Fire2.png">
+                <h1 class="inline-block">
+                  No.5
+                </h1>
+              </div>
+            </div> 
+            <!-- 右 -->
+            <div class="inline-brock">
+              <div class="ml-3">
+                Your heat is cheap!
+              </div>
+              <img class="inline-block ml-1 pl-3" src="../Assets/per.png" alt="完了">
+              <div class="ml-3 inline-block rounded-full align-middle">Cat</div>              
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
